344543caa004a3482bafcc2c30535384401e38af
albialikaj <albialik91@gmail.com>
2020-11-02 16:13:34 +0200

    MVC Project (Controller & View for listing all available sources)

---
+++ BitcoinLogger.Web/Controllers/BitcoinEntryController.cs
@@ -0,0 +1,31 @@
+﻿using BitcoinLogger.Entites;
+using BitcoinLogger.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitcoinLogger.Web.Controllers
+{
+    public class BitcoinEntryController : Controller
+    {
+        // GET: BitcoinEntry
+        public async Task<ActionResult> Index()
+        {
+            ApiService repository = new ApiService();
+            List<BitcoinEntry> coins = new List<BitcoinEntry>();
+
+            BitcoinEntry Gdax = await repository.GetGdaxAsync();
+            BitcoinEntry Bitstamp =  await repository.GetBitstampAsync();     
+            BitcoinEntry Coindesk =  await repository.GetCoindeskAsync();
+
+            coins.Add(Bitstamp);
+            coins.Add(Gdax);
+            coins.Add(Coindesk);
+
+            return View(coins);
+        }
+    }
+}
